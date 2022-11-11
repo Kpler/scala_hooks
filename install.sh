03e54094ed0414b8381dddd5af25b54434f84c17
Germain Geoffroy <ggeoffroy@kpler.com>
2022-11-11 11:06:17 +0800

    chore: upgrade scalafix-cli to 0.10.4

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 ./coursier bootstrap org.scalameta:scalafmt-cli_2.12:2.3.1   -r sonatype:snapshots   -o ./scalafmt --standalone --main org.scalafmt.cli.Cli
-./coursier bootstrap ch.epfl.scala:scalafix-cli_2.12.10:0.9.7 -f --main scalafix.cli.Cli -o scalafix
+./coursier bootstrap ch.epfl.scala:scalafix-cli_2.13.10:0.10.4 -f --main scalafix.cli.Cli -o scalafix
