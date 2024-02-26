f227ced0a229916ba3c0c2d40f4cf82ad8af528f
Léo <ldureuil@kpler.com>
2024-02-26 11:44:27 +0800

    chore: upgrade nix

--- shell.nix
+++ shell.nix
@@ -1,16 +1,16 @@
 let
 
   nixpkgs = builtins.fetchTarball {
-    name   = "nixos-22.11-20230513";
-    url    = "https://github.com/NixOS/nixpkgs/archive/9656e85a15a0.tar.gz";
-    sha256 = "1wwdzv02fjb9gsdyahrfq8bs4y16lp2h08hk6mf9bi52286z9b4r";
+    name   = "nixos-23.05-20231026-051829";
+    url    = "https://github.com/NixOS/nixpkgs/archive/04f431fe64a5.tar.gz";
+    sha256 = "05lwii2zf4j790m886jqag4g5pgahjfg9balvfifah3aic8djzll";
   };
-
 
   pkgs = import nixpkgs { };
 in
   pkgs.mkShell {
     buildInputs = [
       pkgs.coursier
+      pkgs.pre-commit
     ];
   }
