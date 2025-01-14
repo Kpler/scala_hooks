7f976cfbc05eadd19d055ed9bff90e260b81e17e
Léo <ldureuil@kpler.com>
2025-01-14 15:29:33 +0800

    chore: upgrade nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
 let
-
   nixpkgs = builtins.fetchTarball {
-    name   = "nixos-23.05-20231026-051829";
-    url    = "https://github.com/NixOS/nixpkgs/archive/04f431fe64a5.tar.gz";
-    sha256 = "05lwii2zf4j790m886jqag4g5pgahjfg9balvfifah3aic8djzll";
+    name   = "nixos-24.05-20241112";
+    url    = "https://github.com/NixOS/nixpkgs/archive/9256f7c71a19.tar.gz";
+    sha256 = "0dxwisjg7cg8m7lm1643s0fs9gplji1l25hiwplkqwa5c4hs6v5b";
   };
 
   pkgs = import nixpkgs { };
