784fe56f6213b030df22e91c3791974ce28d7e18
Léo <ldureuil@kpler.com>
2025-01-14 15:32:00 +0800

    chore: drop scalafix (should be run directly from sbt)
    tag: #major

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
 ./coursier bootstrap org.scalameta:scalafmt-cli_2.12:2.3.1   -r sonatype:snapshots   -o ./scalafmt --standalone --main org.scalafmt.cli.Cli
-./coursier bootstrap ch.epfl.scala:scalafix-cli_2.13.10:0.10.4 -f --main scalafix.cli.Cli -o scalafix
